dec0389cc5364e8353628fc2690617a2441deb0a
Bhawkins94 <brandon.n.hawkins@gmail.com>
2020-02-29 18:13:06 -0600

    menu bar

--- talentmap/src/App.js
+++ talentmap/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-
-
-
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <nav>
+            <ul>
+                <li>Dashboard</li>
+                <li>Surveys</li>
+                <li>Reports</li>
+                <li>Feedback</li>
+                <li>Action Planning</li>
+                <li>Admin</li>
+            </ul>
+        </nav>
+
       </header>
     </div>
   );
