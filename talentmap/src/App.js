9a2afc76c5b7fde0155a595038532d52e388c2f4
Bhawkins94 <brandon.n.hawkins@gmail.com>
2020-03-01 13:23:03 -0600

    modified:   App.css 	modified:   App.js

--- talentmap/src/App.js
+++ talentmap/src/App.js
@@ -6,13 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
        <nav>
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
             <ul>
                 <li>Dashboard</li>
                 <li>Surveys</li>
                 <li>Reports</li>
                 <li>Feedback</li>
-                <li>Action Planning</li>
+                <li class="wider">Action Planning</li>
                 <li>Admin</li>
+	        <li><i class="material-icons">language</i></li>
+	        <li><i class="material-icons">account_circle</i></li>
             </ul>
         </nav>
 
