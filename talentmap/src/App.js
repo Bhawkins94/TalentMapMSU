9420ef25284d5fea3107b643f61f74df745b7baa
Violet <43896043+shriekingviolet@users.noreply.github.com>
2020-03-02 14:45:10 -0600

    Styling on dashboard
    We're gonna have to redo this bar lol

--- talentmap/src/App.js
+++ talentmap/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
+import LanguageIcon from '@material-ui/icons/Language';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <nav>
-	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
             <ul>
                 <li>Dashboard</li>
                 <li>Surveys</li>
                 <li>Reports</li>
-                <li>Feedback</li>
-                <li class="wider">Action Planning</li>
+                <li id="selectedPage">Feedback</li>
+                <li>Action Planning</li>
                 <li>Admin</li>
-	        <li><i class="material-icons">language</i></li>
-	        <li><i class="material-icons">account_circle</i></li>
+				<li><LanguageIcon style={{ fontSize: 14 }}/></li>
+				<li><AccountCircleIcon style={{ fontSize: 14}}/></li>
             </ul>
         </nav>
 
