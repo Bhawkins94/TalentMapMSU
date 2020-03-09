8db82f3adaa240717c05c12199c1f28fb4f8c699
Bhawkins94 <brandon.n.hawkins@gmail.com>
2020-03-09 15:15:08 -0500

    Changes to be committed: 	new file:   PDF/StyleGuide.pdf 	modified:   talentmap/src/App.css 	modified:   talentmap/src/App.js   Changes not staged for commit: 	deleted:    PDF/_ Style Guide.pdf

--- talentmap/src/App.js
+++ talentmap/src/App.js
@@ -2,26 +2,31 @@ import React from 'react';
 import './App.css';
 import LanguageIcon from '@material-ui/icons/Language';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-
+import AcUnitIcon from '@material-ui/icons/AcUnit';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <nav>
+        <nav>
             <ul>
+                <li><AcUnitIcon/></li>  {/*get talent map icon for here*/}
                 <li>Dashboard</li>
                 <li>Surveys</li>
                 <li>Reports</li>
                 <li id="selectedPage">Feedback</li>
                 <li>Action Planning</li>
                 <li>Admin</li>
-				<li><LanguageIcon style={{ fontSize: 14 }}/></li>
-				<li><AccountCircleIcon style={{ fontSize: 14}}/></li>
+				<li style={{float: 'right'}} ><LanguageIcon /></li>
+				<li style={{float: 'right'}} ><AccountCircleIcon /></li>
             </ul>
         </nav>
-
       </header>
+        <div>
+
+            <h1 class="placeholder">Inserting other elements here</h1>
+        </div>
     </div>
+
   );
 }
 
